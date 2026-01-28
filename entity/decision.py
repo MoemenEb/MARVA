e6fb60212df9da45297aa1648fb6723dc33ab719
Moemen Ebrahim <mon@Moemens-MacBook-Pro.local>
2026-01-28 13:56:50 +0200

    update s2

--- entity/decision.py
+++ entity/decision.py
@@ -13,3 +13,10 @@ class Decision:
             "Duration" : self.duration,
             "Validation" : self.decision
         }
+    
+    def set_decision(self, requirement_set):
+        self.decision = (
+            [req.to_dict() for req in requirement_set.requirements]
+            if self.mode == "single"
+            else requirement_set.to_dict()
+        )
